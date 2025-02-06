9430e4e44b55cd6d95dadee57b4943205279b2fc
rannn367 <alicia.chang.chuo.ran@gmail.com>
2025-02-06 13:28:40 +0800

    Add mark as done command and new class Task

--- src/main/java/Nyanko.java
+++ src/main/java/Nyanko.java
@@ -10,9 +10,11 @@ public class Nyanko {
                 + "ok fine... your task has been added! \n"
                 + "added: ";
         String toDoListMessage = "ok you funny soul here's your to do list:\n";
+        String markAsDoneMessage = "zzzz... oh WHAT you're done already?\n";
+        String markAsNotDoneMessage = "I knew it! You're not done!\n";
         System.out.println(greeting);
 
-        String[] toDoList = new String[100];
+        Task[] toDoList = new Task[100];
         int index = 0;
 
         //Solution for scanner input inspired by https://www.geeksforgeeks.org/how-to-take-input-from-user-in-java/
@@ -25,12 +27,29 @@ public class Nyanko {
             } else if (str.equals("list")) {
                 System.out.println(toDoListMessage);
                 for (int i = 0; i < index; i++) {
-                    System.out.println((i + 1) + ". " + toDoList[i]);
+                    String nextToDoListMessage = (i + 1)
+                            + ". ["
+                            + toDoList[i].getStatusIcon()
+                            + "] "
+                            + toDoList[i].getDescription();
+                    System.out.println(nextToDoListMessage);
                 }
+            } else if (str.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(str.substring(5)) - 1;
+                toDoList[taskIndex].markAsDone();
+                System.out.println(markAsDoneMessage);
+                System.out.println("  [" + toDoList[taskIndex].getStatusIcon() + "] "
+                        + toDoList[taskIndex].getDescription());
+            } else if (str.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(str.substring(7)) - 1;
+                toDoList[taskIndex].markAsNotDone();
+                System.out.println(markAsNotDoneMessage);
+                System.out.println("  [" + toDoList[taskIndex].getStatusIcon() + "] "
+                        + toDoList[taskIndex].getDescription());
             } else {
-                System.out.println(listMessage + str);
-                toDoList[index] = str;
-                index++;
+                    System.out.println(listMessage + str);
+                    toDoList[index] = new Task(str);
+                    index++;
             }
         }
 
