a3dde00da1709b186e7e1647e4ae4b866f96ae3c
rannn367 <alicia.chang.chuo.ran@gmail.com>
2025-02-06 11:28:17 +0800

    Change Duke to Nyanko

---
+++ src/main/java/Nyanko.java
@@ -0,0 +1,9 @@
+public class Nyanko {
+    public static void main(String[] args) {
+        String name = "NYANKO\n";
+        String message = "Hello from " + name
+                + "What can I do for you?\n \n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(message);
+    }
+}
