ce6e386fb00937c4962fa6e6690a9522a5a258d4
rannn367 <alicia.chang.chuo.ran@gmail.com>
2025-02-06 11:53:16 +0800

    Add echo and quit command

--- src/main/java/Nyanko.java
+++ src/main/java/Nyanko.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Nyanko {
     public static void main(String[] args) {
         String name = "NYANKO\n";
-        String message = "Hello from " + name
-                + "What can I do for you?\n \n"
-                + "Bye. Hope to see you again soon!";
-        System.out.println(message);
+        String greeting = "HEEHAW I'M " + name
+                + "Today's a good day to chill and slack!\n"
+                + "zzzzzz";
+        System.out.println(greeting);
+
+        //Solution below inspired by https://www.geeksforgeeks.org/how-to-take-input-from-user-in-java/
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            String str = scn.nextLine();
+            if (str.equals("bye")) {
+                System.out.println("Good night... I'm going to nap zzzzz");
+                break;
+            }
+            System.out.println(str);
+        }
     }
 }
