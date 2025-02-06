f8438512d0d7957abd3f77efce01b0f8d43c8bc4
rannn367 <alicia.chang.chuo.ran@gmail.com>
2025-02-06 14:13:42 +0800

    Add Deadline, Event, ToDo classes that inherits Task

--- src/main/java/Nyanko.java
+++ src/main/java/Nyanko.java
@@ -12,6 +12,7 @@ public class Nyanko {
         String toDoListMessage = "ok you funny soul here's your to do list:\n";
         String markAsDoneMessage = "zzzz... oh WHAT you're done already?\n";
         String markAsNotDoneMessage = "I knew it! You're not done!\n";
+        String othersMessage = "Whot on earth are you saying!!!\n";
         System.out.println(greeting);
 
         Task[] toDoList = new Task[100];
@@ -28,28 +29,43 @@ public class Nyanko {
                 System.out.println(toDoListMessage);
                 for (int i = 0; i < index; i++) {
                     String nextToDoListMessage = (i + 1)
-                            + ". ["
-                            + toDoList[i].getStatusIcon()
-                            + "] "
-                            + toDoList[i].getDescription();
+                            + ". "
+                            + toDoList[i].toString();
                     System.out.println(nextToDoListMessage);
                 }
             } else if (str.startsWith("mark ")) {
                 int taskIndex = Integer.parseInt(str.substring(5)) - 1;
                 toDoList[taskIndex].markAsDone();
                 System.out.println(markAsDoneMessage);
-                System.out.println("  [" + toDoList[taskIndex].getStatusIcon() + "] "
-                        + toDoList[taskIndex].getDescription());
+                System.out.println("  " + toDoList[taskIndex].toString());
             } else if (str.startsWith("unmark ")) {
                 int taskIndex = Integer.parseInt(str.substring(7)) - 1;
                 toDoList[taskIndex].markAsNotDone();
                 System.out.println(markAsNotDoneMessage);
-                System.out.println("  [" + toDoList[taskIndex].getStatusIcon() + "] "
-                        + toDoList[taskIndex].getDescription());
+                System.out.println("  " + toDoList[taskIndex].toString());
+            } else if (str.startsWith("deadline ")) {
+                System.out.println("When is it due?");
+                String by = scn.nextLine();
+                toDoList[index] = new Deadline(str.substring(9), by);
+                System.out.println(listMessage + toDoList[index].toString());
+                System.out.println("Oh my! You have " + (index + 1) + " tasks!");
+                index++;
+            } else if (str.startsWith("todo ")) {
+                toDoList[index] = new ToDo(str.substring(5));
+                System.out.println(listMessage + toDoList[index].toString());
+                System.out.println("Oh my! You have " + (index + 1) + " tasks!");
+                index++;
+            } else if (str.startsWith("event ")) {
+                System.out.println("When does it start?");
+                String from = scn.nextLine();
+                System.out.println("When does it end?");
+                String to = scn.nextLine();
+                toDoList[index] = new Event(str.substring(6), from, to);
+                System.out.println(listMessage + toDoList[index].toString());
+                System.out.println("Oh my! You have " + (index + 1) + " tasks!");
+                index++;
             } else {
-                    System.out.println(listMessage + str);
-                    toDoList[index] = new Task(str);
-                    index++;
+                System.out.println(othersMessage);
             }
         }
 
