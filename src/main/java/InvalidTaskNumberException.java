895e3c30d42210a37132f3b0eb850ab99a7451b5
rannn367 <alicia.chang.chuo.ran@gmail.com>
2025-02-08 05:27:16 +0800

    Add exception handling

---
+++ src/main/java/InvalidTaskNumberException.java
@@ -0,0 +1,8 @@
+public class InvalidTaskNumberException extends Exception {
+    int argument;
+    int numberOfTasks;
+
+    public InvalidTaskNumberException(String message) {
+        super(message);
+    }
+}
--- src/main/java/Nyanko.java
+++ src/main/java/Nyanko.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Nyanko {
-    public static void main(String[] args) {
-        String name = "NYANKO\n";
-        String greeting = "HEEHAW I'M " + name
-                + "Today's a good day to chill and slack!\n"
-                + "zzzzzz";
-        String listMessage = "WOW you're so hardworking\n"
-                + "ok fine... your task has been added! \n"
-                + "added: ";
-        String toDoListMessage = "ok you funny soul here's your to do list:\n";
-        String othersMessage = "Whot on earth are you saying!!!\n";
-        System.out.println(greeting);
+    static String name = "NYANKO\n";
+    static String greeting = "HEEHAW I'M " + name
+            + "Today's a good day to chill and slack!\n"
+            + "zzzzzz";
+    static String listMessage = "WOW you're so hardworking\n"
+            + "ok fine... your task has been added! \n"
+            + "added: ";
+    static String toDoListMessage = "ok you funny soul here's your to do list:\n";
+    static String othersMessage = "Whot on earth are you saying!!!\n";
 
-        Task[] toDoList = new Task[100];
-        int index = 0;
+    static Task[] toDoList = new Task[100];
+    static int index = 0;
+
+    public static void main(String[] args) {
+        System.out.println(greeting);
 
         //Solution for scanner input inspired by https://www.geeksforgeeks.org/how-to-take-input-from-user-in-java/
         Scanner scn = new Scanner(System.in);
@@ -42,14 +43,29 @@ public class Nyanko {
                 break;
 
             case "mark":
-                int taskIndex = Integer.parseInt(argument) - 1;
-                markAsDone(toDoList[taskIndex]);
-                break;
+                int taskIndex;
+                taskIndex = Integer.parseInt(argument) - 1;
+                try {
+                    validateTaskNumber(taskIndex);
+                    markAsDone(toDoList[taskIndex]);
+                    break;
+                } catch (InvalidTaskNumberException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    break;
+                }
 
             case "unmark":
                 taskIndex = Integer.parseInt(argument) - 1;
-                markAsNotDone(toDoList[taskIndex]);
-                break;
+                try {
+                    validateTaskNumber(taskIndex);
+                    markAsNotDone(toDoList[taskIndex]);
+                    break;
+                } catch (InvalidTaskNumberException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    break;
+                }
 
             case "deadline":
                 System.out.println("When is it due?");
@@ -99,5 +115,12 @@ public class Nyanko {
         task.markAsNotDone();
         System.out.println(markAsNotDoneMessage);
         System.out.println("  " + task.toString() + "\n");
+    }
+
+    public static void validateTaskNumber(int taskIndex) throws InvalidTaskNumberException {
+        if (index == 0 || taskIndex > index || taskIndex < 0) {
+            throw new InvalidTaskNumberException("You are cuckoo! There's only "
+                    + index + " tasks! Task number " + (taskIndex + 1) + " is invalid!!");
+        }
     }
 }
